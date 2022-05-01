a416f4888bd6249fcdbe330ef39aad5a16de7af5
Marc pr <marc.rene@web.de>
2022-05-01 23:34:30 +0200

    implement request on openweather.com

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,4 +1,53 @@
-/* Global Variables */
+// WEB API 
+
+let ZipURL = 'http://api.openweathermap.org/geo/1.0/zip?zip='; // {zip code},{country code}&appid={API key}'
+let WeatherURL= 'https://api.openweathermap.org/data/2.5/weather?lat=';
+let apiKey = '&appid=a448a4a826b1fca8eaa50dcd50dbc65d&units=imperial';
+
+
+
+document.getElementById('generate').addEventListener('click', performAction);
+
+function performAction(e) {
+  const enteredZip = document.getElementById('zip').value;
+  getZipData(ZipURL, enteredZip, apiKey)
+
+  .then((lat, lon) => {
+  getWeatherData(WeatherURL, lat, lon, apiKey)
+  });
+}
+
+const getZipData = async (ZipURL, enteredZip, apiKey) => {
+
+  const res = await fetch (ZipURL+enteredZip+apiKey)
+
+    try {
+      const data = await res.json();
+      // console.log(data);
+      const lat = data.lat;
+      const lon = '&lon='+data.lon;
+      
+    }
+    catch(error) {
+      console.log("error", error);
+  }
+}
+
+
+
+const getWeatherData = async (WeatherURL, lat, lon, apiKey) => {
+
+  const res = await fetch (WeatherURL+lat+lon+apiKey)
+
+    try {
+      const data = await res.json();
+      
+    
+    }
+    catch(error) {
+      console.log("error", error);
+  }
+}
 
 // Create a new date instance dynamically with JS
 let d = new Date();
@@ -26,7 +75,24 @@ const postData = async ( url = '', data = {})=>{
       }
   }
 
+  const retrieveData = async () =>{
+    const request = await fetch('/all');
+    try {
+    // Transform into JSON
+    const allData = await request.json()
+    console.log(allData)
+    // Write updated data to DOM elements
+    document.getElementById('temp').innerHTML = Math.round(allData.temp)+ 'degrees';
+    document.getElementById('content').innerHTML = allData.feel;
+    document.getElementById("date").innerHTML =allData.date;
+    }
+    catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+   }
+
     // TODO-Call Function
   
-    postData('/Animal', {animal:'lion'});
-    postData('/Animal', {animal:'tiger'})
+    // postData('/data', {temperature:34, date: newDate, userResponse:'Hello'});
+    // postData('/data', {animal:'tiger'})
