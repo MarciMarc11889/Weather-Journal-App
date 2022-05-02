dae7442c25f0c16879f0e53187b58538572088ca
Wiediger <mwiedig@emea.corpdir.net>
2022-05-02 15:06:59 +0200

    First Request working

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,10 +1,11 @@
 // WEB API 
 
-let ZipURL = 'http://api.openweathermap.org/geo/1.0/zip?zip='; // {zip code},{country code}&appid={API key}'
-let WeatherURL= 'https://api.openweathermap.org/data/2.5/weather?lat=';
-let apiKey = '&appid=a448a4a826b1fca8eaa50dcd50dbc65d&units=imperial';
-
-
+let ZipURL = 'http://api.openweathermap.org/geo/1.0/zip?zip=', // {zip code},{country code}&appid={API key}'
+    WeatherURL= 'https://api.openweathermap.org/data/2.5/weather?lat=',
+    apiKey = '&appid=a448a4a826b1fca8eaa50dcd50dbc65d&units=imperial',
+    lat = 0, 
+    lon = 0,
+    geoData = {lat, lon};
 
 document.getElementById('generate').addEventListener('click', performAction);
 
@@ -12,36 +13,45 @@ function performAction(e) {
   const enteredZip = document.getElementById('zip').value;
   getZipData(ZipURL, enteredZip, apiKey)
 
-  .then((lat, lon) => {
-  getWeatherData(WeatherURL, lat, lon, apiKey)
+  .then(() => {
+  getWeatherData(WeatherURL, geoData.lat, geoData.lon, apiKey)
+
+  .then(data => console.log(data))
+  
   });
+  
 }
 
 const getZipData = async (ZipURL, enteredZip, apiKey) => {
-
+ 
   const res = await fetch (ZipURL+enteredZip+apiKey)
 
     try {
       const data = await res.json();
-      // console.log(data);
-      const lat = data.lat;
-      const lon = '&lon='+data.lon;
-      
+
+     lat = data.lat;
+     lon = data.lon;
+
+     geoData= {
+       lat,
+       lon
+     };
+
+      return geoData;
     }
     catch(error) {
       console.log("error", error);
   }
 }
 
-
-
 const getWeatherData = async (WeatherURL, lat, lon, apiKey) => {
+ 
 
-  const res = await fetch (WeatherURL+lat+lon+apiKey)
-
+  const res = await fetch (WeatherURL+lat+'&lon='+lon+apiKey)
+  
     try {
       const data = await res.json();
-      
+      return data;
     
     }
     catch(error) {
