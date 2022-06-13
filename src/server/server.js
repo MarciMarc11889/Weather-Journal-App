f7441beddff1c6048f52b0a2066a4933af503794
Marc pr <marc.rene@web.de>
2022-06-13 22:24:38 +0200

    added all APIs

--- src/server/server.js
+++ src/server/server.js
@@ -29,28 +29,104 @@ app.use(express.static('dist'));
 
 // Function to fetch Latitude and Longitude from geoNamesURL.org
 const   geoNamesURL = 'http://api.geonames.org/searchJSON?',
-        username    = 'marcimarc11889'
+        username    = 'marcimarc11889',
+        name        = 'Berlin'
+let     geo = {
+          lat:0,
+          long:0
+        }
 
-// http://api.geonames.org/searchJSON?q=london&maxRows=10&username=marcimarc11889
-
-const getLatLon = async (name='', geoNamesURL, username ) => {
+const getLatLon = async (name, geoNamesURL, username ) => {
   await fetch(`${geoNamesURL}q=${name}&maxRows=1&username=${username}`) 
     .then(res => { 
         const body = res.json();
         return body
     })
-    .then(body => {
-      const long = body.geonames[0].lng
-      const lat = body.geonames[0].lat
-      const geo = {long, lat}
-      return geo
+    .then((body) => {
+      geo.long = body.geonames[0].lng
+      geo.lat = body.geonames[0].lat
+      console.log(`The Geo Data is: ${geo}`)
     })
-    .then(geo =>{
-      console.log(geo)
+    .catch(error => {
+      console.log("error", error);
+    })
+} 
+
+// Function to fetch Current Weather from weatherbit.io
+const   CurrentWeatherURL = 'https://api.weatherbit.io/v2.0/current?', //Only supports GET request
+        apiKeyWeatherbitkey='2130296c39a7402da71ece28eff267f9'
+
+//example: https://api.weatherbit.io/v2.0/current?lat=35.7796&lon=-78.6382&key=API_KEY&include=minutely
+
+
+const getCurrentWeather = async () => {
+  await fetch(`${CurrentWeatherURL}&lat=${geo.lat}&lon=${geo.long}&key=${apiKeyWeatherbitkey}`) 
+    .then(res => { 
+        const body = res.json();
+        return body
+    })
+    .then(body => {
+      console.log(`The current weather is: ${body}`)
+    })
+    .catch(error => {
+      console.log("error", error);
     })
 }
 
-getLatLon('London', geoNamesURL, username)
+const   FutureURL = 'https://api.weatherbit.io/v2.0/forecast/daily?'
+
+// example:https://api.weatherbit.io/v2.0/forecast/daily?city=Raleigh,NC&key=API_KEY
+
+const getFutureWeather = async () => {
+  await fetch(`${FutureURL}&lat=${geo.lat}&lon=${geo.long}&key=${apiKeyWeatherbitkey}`) 
+    .then(res => { 
+        const body = res.json();
+        return body
+    })
+    .then(body => {
+      console.log(`The future weather is ${body}`)
+    })
+    .catch(error => {
+      console.log("error", error);
+    })
+}
+
+// example: https://pixabay.com/api/?key=27995832-6c0e40861203c9351e312e73c&q=yellow+flowers&image_type=photo
+
+const apiKeyPixabay='key=27995832-6c0e40861203c9351e312e73c&',
+      pixaBayURL ='https://pixabay.com/api/?',
+      destination= `q=${name}`
+
+const getPic = async () => {
+  await fetch(`${pixaBayURL}${apiKeyPixabay}${destination}&image_type=photo`) 
+    .then(res => { 
+        const body = res.json();
+        return body
+    })
+    .then(body => {
+      console.log(`The example picture: ${body.hits[0]}`)
+    })
+    .catch(error => {
+      console.log("error", error);
+    })
+}
+
+
+const action = async ()=>{
+  await getLatLon(name, geoNamesURL, username)
+  .then(()=>{
+    getCurrentWeather()
+  })
+  .then(()=>{
+    getFutureWeather()
+  })
+  .then(()=>{
+    getPic()
+  })
+
+}
+
+action()
 
 // Setup Server
 
