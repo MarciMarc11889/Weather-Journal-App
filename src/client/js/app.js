4221ad6dcf330ac482192d18faa9070a25fbc159
Marc pr <marc.rene@web.de>
2022-06-17 23:23:38 +0200

    startcode of app.js

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -5,30 +5,19 @@ let d = new Date();
 let newDate = d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear();
 
 // Listen to "Generate" Button
-document.getElementById("generate").addEventListener("click", performAction);
+document.getElementById("submit").addEventListener("click", performAction);
 
 // Function after clicking on "Generate"
 function performAction(e) {
-  const enteredZip = document.getElementById("zip").value;
-  const feel = document.getElementById("feelings").value;
+  const APIdata= {
+    destination : document.getElementById("dest").value,
+    date : document.getElementById("date").value
+  } 
+  console.log(APIdata)
+  postData('/data', APIdata)
 
-  //   //Remove fade class from DOM elements
-  document.getElementById("temp").classList = "";
-  document.getElementById("content").classList = "";
-  document.getElementById("date").classList = "";
 
-  getZipData(ZipURL, enteredZip, apiKey)
-    .then(() => getWeatherData(WeatherURL, geoData.lat, geoData.lon, apiKey))
-
-    .then(data => {
-      const getTemp = data.main.temp;
-      return getTemp;
-    })
-
-    .then(getTemp => postData("/data", { getTemp, newDate, feel }))
-    .then(() => retrieveData());
 }
-
 /* Function to POST data */
 const postData = async (url = "", data = {}) => {
   console.log(data);
@@ -49,25 +38,27 @@ const postData = async (url = "", data = {}) => {
     console.log("error", error);
   }
 };
-// retrieve data from localhost and update UI
-const retrieveData = async () => {
-  const request = await fetch("/all");
-  try {
-    // Transform into JSON
-    const allData = await request.json();
-    console.log(allData);
-    // Write updated data to DOM elements
-    document.getElementById("temp").innerHTML =
-      Math.round(allData.temp) + " degrees";
-    document.getElementById("temp").classList = "fade";
-    document.getElementById("content").innerHTML = allData.feel;
-    document.getElementById("content").classList = "fade";
-    document.getElementById("date").innerHTML = allData.date;
-    document.getElementById("date").classList = "fade";
-  } catch (error) {
-    console.log("error", error);
-    // appropriately handle the error
-  }
-};
+
+
+// // retrieve data from localhost and update UI
+// const retrieveData = async () => {
+//   const request = await fetch("/all");
+//   try {
+//     // Transform into JSON
+//     const allData = await request.json();
+//     console.log(allData);
+//     // Write updated data to DOM elements
+//     document.getElementById("temp").innerHTML =
+//       Math.round(allData.temp) + " degrees";
+//     document.getElementById("temp").classList = "fade";
+//     document.getElementById("content").innerHTML = allData.feel;
+//     document.getElementById("content").classList = "fade";
+//     document.getElementById("date").innerHTML = allData.date;
+//     document.getElementById("date").classList = "fade";
+//   } catch (error) {
+//     console.log("error", error);
+//     // appropriately handle the error
+//   }
+// };
 
 export { performAction }
