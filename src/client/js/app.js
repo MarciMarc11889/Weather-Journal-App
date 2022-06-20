a25d59a68ca6958ebaf13999e7efcb39e7f70acd
Marc Wiediger <marc.rene@web.de>
2022-06-20 16:37:41 +0200

    Changed button click to async func

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -6,13 +6,15 @@ let newDate = d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear();
 
 
 // Function after clicking on "Generate". Eventlistener on HTML-side
-function performAction(e) {
+async function performAction(e) {
   const APIdata = {
     destination: document.getElementById("dest").value,
     date: document.getElementById("date").value
   }
   console.log(APIdata)
-  Client.postData("//localhost:3000/data", APIdata)
+  await Client.postData("//localhost:3000/data", APIdata)
+  .then(() =>{console.log(Client.newData)
+  })
 }
 
 export { performAction }
