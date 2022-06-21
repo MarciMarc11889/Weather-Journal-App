a9f453acf5f4d5bfe2b68120cc8fcfc87304f4ce
Marc Wiediger <marc.rene@web.de>
2022-06-21 12:56:50 +0200

    Put pic into div element

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -15,18 +15,24 @@ async function performAction(e) {
   await Client.postData("//localhost:3000/data", APIdata)
   .then(() =>{
     const pic = Client.newData
-    console.log(pic.picture)
     return pic
   })
-  .then(async (pic) => {
+  .then((pic) => {
     console.log(pic.picture)
-    let x = document.createElement("IMG")
+    const existingPic  = document.getElementById("pic")
+    if (existingPic.hasChildNodes()) {
+      document.getElementById("picture").remove()
+    }
+  
+    
+   
+    const x = document.createElement("IMG")
     x.setAttribute("src", pic.picture)
     x.setAttribute("width", "304")
     x.setAttribute("height", "228")
     x.setAttribute("id", "picutre")
     x.setAttribute("alt", "Picture of city")
-  document.body.appendChild(x)
+  document.getElementById("pic").appendChild(x)
   })
 }
 
