6804e43c36ce24f001ff1972b58a249b0652d16b
Marc Wiediger <marc.rene@web.de>
2022-06-22 14:59:37 +0200

    Moved date constructor to server side

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -1,10 +1,4 @@
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth() + 1 + "/" + d.getDate() + "/" + d.getFullYear();
-
-
-
 // Function after clicking on "Submit". Eventlistener on HTML-side
 async function performAction(e) {
   const APIdata = {
