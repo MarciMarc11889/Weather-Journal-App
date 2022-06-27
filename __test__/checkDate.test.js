948462e2f8224f3bee5cc96e955c179278981cfd
Marc pr <marc.rene@web.de>
2022-06-27 18:53:02 +0200

    Added testfiles for jest

---
+++ __test__/checkDate.test.js
@@ -0,0 +1,7 @@
+import {checkDate} from "../src/server/index"
+
+
+describe("Testing the submit functionality", () => {
+    test("Testing the action() function", () => {
+        expect(checkDate).toBeDefined();
+    })});
---
+++ __test__/initDateSelector.test.js
@@ -0,0 +1,7 @@
+import {setDateLimits} from "../src/client/js/initDateSelector"
+
+
+describe("Testing the submit functionality", () => {
+    test("Testing the postData() function", () => {
+        expect(setDateLimits).toBeDefined();
+    })});
