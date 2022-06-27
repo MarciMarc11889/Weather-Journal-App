26f816acd440ae480fa5947678fed3b2165a9743
Marc Wiediger <marc.rene@web.de>
2022-06-27 14:06:26 +0200

    Added function to limit Dateselector

--- src/client/index.js
+++ src/client/index.js
@@ -1,10 +1,12 @@
 import { performAction } from './js/app'
+import { setDateLimits } from './js/initDateSelector'
 import { postData } from './js/postData'
 import {newData} from './js/postData'
 
 
 import './styles/style.scss'
 
+setDateLimits()
 
 export { performAction };
 export { postData }
---
+++ src/client/js/initDateSelector.js
@@ -0,0 +1,20 @@
+//Set Date Selector to min=0 Days and max= +16 days
+
+const setDateLimits = () => {
+let getDate         = new Date(),
+    year            = getDate.getFullYear(),
+    month           = getDate.getMonth(),
+    day             = getDate.getDate(),
+    futureDate      = new Date(year,month,day +16),
+    futureYear      = futureDate.getFullYear(),
+    futureDay       = futureDate.getDate(),
+    futureMonth     = futureDate.getMonth(),
+    today           = `${year}-0${month + 1}-${day}`,
+    future          = `${futureYear}-0${futureMonth + 1}-${futureDay}`
+
+const date = document.getElementById('date')
+date.setAttribute("min", today)
+date.setAttribute("max", future)
+}
+
+export {setDateLimits}
